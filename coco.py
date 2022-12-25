d50e371297e0e74f04b04f906fefe5d51cf403eb
jsk1107 <seungkyu.korea@gmail.com>
2022-12-25 15:02:37 +0900

    add func of adj_category

--- coco.py
+++ coco.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Original author: jsk1107
+
 import json
 from pycocotools.coco import COCO
 import time
@@ -20,9 +24,9 @@ class CocoUtils(COCO):
         self.dataset = dataset
         self.createIndex()
 
-    def del_category(self, catIds=[], catNms=[]):
-        catIds = catIds if self._isArrayLike(catIds) else [catIds]
+    def del_category(self, catNms=[], catIds=[]):
         catNms = catNms if self._isArrayLike(catNms) else [catNms]
+        catIds = catIds if self._isArrayLike(catIds) else [catIds]
 
         if (len(catIds) == 0 and len(catNms) == 0) or (len(catIds) != 0 and len(catNms) != 0):
             raise Exception('Only one of catIds and catNms can be received as an argument.')
@@ -31,7 +35,7 @@ class CocoUtils(COCO):
         annotations = self.dataset['annotations']
 
         if len(catIds) == 0 and len(catNms) != 0:
-            catIds = self.getCatIds(catNms=catNms)
+            catIds = self.getCatIds(catNms=[catNms])
 
         cnt = 0
         # del category in categories
@@ -54,9 +58,50 @@ class CocoUtils(COCO):
         print(f're-indexing...')
         self.createIndex()
 
-    def adj_category(self):
-        pass
+    def adj_category(self, bf_catNms=[], af_catNms=[]):
+        print('Adjust categories ... ')
+        bf_catNms = bf_catNms if self._isArrayLike(bf_catNms) else [bf_catNms]
+        af_catNms = af_catNms if self._isArrayLike(af_catNms) else [af_catNms]
+        if len(bf_catNms) != len(af_catNms):
+            raise Exception('the number of bf_catNms and af_catNms must be equal.')
 
+        categories = self.dataset['categories']
+        annotations = self.dataset['annotations']
+
+        idx = 0
+        while idx <= len(bf_catNms) - 1:
+
+            bf_catNm, af_catNm = bf_catNms[idx], af_catNms[idx]
+            bf_id = self.getCatIds(catNms=[bf_catNm])
+            if len(bf_id) == 0:
+                raise Exception(f'{bf_catNm} does not exist')
+
+            af_id = self.getCatIds(catNms=[af_catNm])
+            if len(af_id) == 0:
+                for category in categories:
+                    if category['name'] in bf_catNm:
+                        category['name'] = af_catNm
+                        print(f'{bf_catNm} change {af_catNm}.')
+                        break
+            else:
+                cnt = 0
+                for annotation in annotations:
+                    if annotation['category_id'] not in bf_id:
+                        continue
+                    annotation['category_id'] = af_id[0]
+                    cnt += 1
+                print(f'"{bf_catNm}": {cnt} annotation info has been adjusted to "{af_catNm}"')
+
+                cat_idx = 0
+                while cat_idx <= len(categories) - 1:
+                    if categories[cat_idx]['id'] in bf_id:
+                        del categories[cat_idx]
+                        continue
+                    cat_idx += 1
+            idx += 1
+
+        print(f're-indexing...')
+        self.createIndex()
     def add_category(self):
         pass
 
@@ -82,12 +127,20 @@ if __name__ == '__main__':
     with open(PATH, 'r') as f:
         dataset = json.load(f)
 
-    # dog, 18
-    coco = CocoUtils(dataset)
-    print(coco.loadCats(20))
-    coco.del_category(20)
-    print(coco.loadCats(20))
+    # dog, 18, person, 20
+    # clock, 85, teddy bear, 88
+    # "supercategory": "vehicle",
+    # "id": 5,
+    # "name": "airplane"
+    #
+    # "supercategory": "vehicle",
+    # "id": 6,
+    # "name": "bus"
 
+    coco = CocoUtils(dataset)
+    coco.adj_category(["clock", "teddy bear"], ["person", "dog"])
+    # print(coco.dataset["categories"])
+    # coco.del_category(20)
     # coco.del_category(catIds=18)
     # print(isinstance(coco, CocoUtils))
     # coco = COCO(PATH)
